4e7adc054d97fcf342b494b089ce6a5aec0fd680
Fernando Hernandez <ferandree270904@gmail.com>
2023-10-05 09:47:59 -0600

    Update 2 Usuario.java
    Se definieron los metodos de la clase

--- Usuario.java
+++ Usuario.java
@@ -63,3 +63,28 @@ public void setEdad(int edad) {
     this.edad = edad;
 }
 
+// Método toString
+@Override
+public String toString() {
+    return "Usuario{" +
+            "id=" + id +
+            ", nombre='" + nombre + '\'' +
+            ", apellido='" + apellido + '\'' +
+            ", email='" + email + '\'' +
+            ", edad=" + edad +
+            '}';
+}
+
+// Metodo para comprar dos objetos Usuario
+@Override
+public boolean equals (Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Usuario usuario = (Usuario) o;
+    return id == usuario.id;
+}
+
+@Override
+public int hashCode() {
+    return Objects.hash(id);
+}
