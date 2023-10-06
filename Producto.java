2247e34abd9c1ee11d1b26277ccb528155545aa5
Fercho1118 <rue23748@uvg.edu.gt>
2023-10-05 21:39:04 -0600

    Implementación de constructores en clase Producto
    Se agregaron los constructores a la clase Producto

--- Producto.java
+++ Producto.java
@@ -4,4 +4,16 @@ public class Producto {
     private String detalles;
     private double costo;
     private boolean disponible;
+
+    public Producto() {
+
+    }
+    
+    public Producto(int idProducto, String titulo, String detalles, double costo, boolean disponible) {
+        this.idProducto = idProducto;
+        this.titulo = titulo;
+        this.detalles = detalles;
+        this.costo = costo;
+        this.disponible = disponible;
+    }    
 }
