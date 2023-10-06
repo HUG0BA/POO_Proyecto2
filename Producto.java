5273d986029c5059eb9e623e0efb768b1924f196
Fercho1118 <rue23748@uvg.edu.gt>
2023-10-06 00:17:34 -0600

    Implementación de getters y setters en clase Productos
    Se añadieron los getters y setters para la clase Producto

--- Producto.java
+++ Producto.java
@@ -5,15 +5,59 @@ public class Producto {
     private double costo;
     private boolean disponible;
 
+    // Constructor predeterminado
     public Producto() {
 
     }
     
+    // Constructor con parámetros
     public Producto(int idProducto, String titulo, String detalles, double costo, boolean disponible) {
         this.idProducto = idProducto;
         this.titulo = titulo;
         this.detalles = detalles;
         this.costo = costo;
         this.disponible = disponible;
-    }    
+    }   
+    
+   //Getters
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDetalles() {
+        return detalles;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+   //Setters
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDetalles(String detalles) {
+        this.detalles = detalles;
+    }
+
+    public void setCosto(Double costo) {
+        this.costo = costo;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
 }
