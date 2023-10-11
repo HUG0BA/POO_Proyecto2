588b6bb0c163ace7cf52e25c4106fe7ef1f8e1c6
HUG0BA <127461683+HUG0BA@users.noreply.github.com>
2023-10-10 19:57:50 -0600

    Create Pedido.java

---
+++ Pedido.java
@@ -0,0 +1,15 @@
+public class Pedido{
+    private int idPedido;
+    private Cliente cliente;
+    private Repartidor repartidor;
+    private Restaurante restaurante;
+    private ArrayList<Producto> productos;
+    private String local;
+    private boolean asignada;
+    private boolean completado;
+
+    private Pedido(Cliente clienteP, Restaurante restaurante, String local){
+        cliente = new Cliente(clienteP.getId(), clienteP.getNombre(), clienteP.getApellido(), clienteP.getEmail(), clienteP.getEdad(), clienteP.getTipo(), clienteP.getTotalP(), clienteP.getTotalPE(), clienteP.getTotalPC, clienteP.getMetPag(), clienteP.getCalif());
+        
+    }
+}
