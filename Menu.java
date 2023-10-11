ab26221a5e45f7b883426bb2494a1aba4f716791
HUG0BA <127461683+HUG0BA@users.noreply.github.com>
2023-10-10 18:33:58 -0600

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -4,6 +4,7 @@ public class Menu{
     private String detalles;
     private double costo;
     private boolean disponible;
+    private 
 
     public Menu(){
 
@@ -17,4 +18,62 @@ public class Menu{
         this.disponible = disponible;
     }
 
+    public int getIdMenu(){
+        return idMenu;
+    }
+
+    public void setIdMenu(int idMenu){
+        this.idMenu = idMenu;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getDetalles(){
+        return detalles;
+    }
+
+    public void setDetalles(String detalles){
+        this.detalles = detalles;
+    }
+
+    public double getCosto(){
+        return this.costo;
+    }
+
+    public void setCosto(double costo){
+        this.costo = costo;
+    }
+
+    public boolean getDisponible(){
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible){
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString(){
+        return "Menu {" + 
+                "idMenu=" + idMenu + 
+                ", titulo= '" + titulo + '\'' +
+                ", detalles='" + detalles + '\'' +
+                ", costo=" + costo +
+                ", disponible=" + disponible +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return idMenu == menu.idMenu;
+    }
 }
