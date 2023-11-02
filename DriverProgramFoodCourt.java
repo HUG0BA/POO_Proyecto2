9d507b7a225efc3d5a98058ef6962319fb43d374
Fercho1118 <rue23748@uvg.edu.gt>
2023-11-02 06:53:55 -0600

    Reescribiendo menuProveedor
    Hubo un error y en al anterior commit se borró el menú, por lo que simplemente se escribió de nuevo.

--- DriverProgramFoodCourt.java
+++ DriverProgramFoodCourt.java
@@ -244,6 +244,50 @@ public class DriverProgramFoodCourt{
             System.out.println()
         }
     }
-    
+
+    public void menuProveedor() {
+        boolean seguirEnMenu = true;
+
+        while (seguirEnMenu) {
+            System.out.println("\n--- Menú del proveedor ---");
+            System.out.println("1. Agregar/eliminar establecimientos");
+            System.out.println("2. Agregar/eliminar opciones del menú");
+            System.out.println("3. Modificar menús existentes");
+            System.out.println("4. Personalizar pantalla principal de un negocio");
+            System.out.println("5. Modificar datos de la pantalla principal");
+            System.out.println("6. Modificar disponiblidad de pedidos");
+            System.out.println("7. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    //Método para agregar/eliminar establecimientos
+                    break;
+                case 2:
+                    //Método para agregar/eliminar opciones del menú
+                    break;
+                case 3:
+                    //Método para modificar menús existentes
+                    break;
+                case 4:
+                    //Método para personalizar pantalla principal de un negocio
+                    break;
+                case 5:
+                    //Método para modificar datos de la pantalla principal
+                    break;
+                case 6:
+                    //Método para modificar disponiblididad de pedidos
+                    break;
+                case 7:
+                    seguirEnMenu = false;
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+
+            }
+        }
+    }
 }
- 
